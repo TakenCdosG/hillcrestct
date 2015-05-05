d5092aec0ab7198265f5d54da273d8fca513f560
caedlesu <caedlesu@gmail.com>
2015-05-05 12:01:11 -0500

    fixed wp-config

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'breakwat_wo5070');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'breakwat_wo5070');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'YiBmvUYqsJaH');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
