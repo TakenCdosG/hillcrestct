9a93be1e49c45c642fd7f915820defe6ae0291fb
Maicol Lopez Mora <you@example.com>
2015-06-29 13:33:43 -0500

    life files

--- wp-content/plugins/store-locator/sl-admin/info/test-curl.php
+++ wp-content/plugins/store-locator/sl-admin/info/test-curl.php
@@ -1,4 +1,4 @@
-<?php$is_included_tc=(basename(__FILE__) != basename($_SERVER['SCRIPT_FILENAME']) )? true : false;if ($is_included_tc) {	if (!function_exists('curl_init')) {		print ("cURL library <span style='color:red'>cannot be found</span>. Make sure it is installed."); 		/*exit;*/	} else {
+<?php$is_included_tc=(basename(__FILE__) != basename($_SERVER['SCRIPT_FILENAME']) )? true : false;if ($is_included_tc) {	if (!function_exists('curl_init')) {		print ("cURL library <span style='color:red'>cannot be found</span>. Make sure it is installed."); 		/*exit;*/	} else {
 		/*$agent = "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4) Gecko/20030624 Netscape/7.1 (ax)";	
 	$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL,"https://www.google.com/adsense/");	
@@ -13,6 +13,6 @@
 	Details Error :<br/><b>".curl_error($ch)."</b></span>, ";
 		} else {	
 		echo "<span style='color:green'>Your cURL is working properly</span>,  ";
-		}
+		}
 		curl_close ($ch);*/		echo "cURL <span style='color:green'>exists</span> on your site,  ";	}	$fgc_exists=(function_exists("file_get_contents"))? "<span style='color:green'>exists" : "<span style='color:red'>doesn't exist" ;	print "'file_get_contents' function $fgc_exists</span>";	}
 ?>
