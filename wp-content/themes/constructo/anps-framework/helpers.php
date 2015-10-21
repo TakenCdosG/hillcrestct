4c74f7259670c732b9c27fa711898b97a2d4517f
Carlos Leaño <an1m3rul3z@gmail.com>
2015-10-21 12:04:06 -0500

    fixed facebook social icon position

--- wp-content/themes/constructo/anps-framework/helpers.php
+++ wp-content/themes/constructo/anps-framework/helpers.php
@@ -193,8 +193,8 @@ function anps_get_header() {
                 ?>
                 <div class="social-icons-header">
                     <div class="social-icons-container">
-                        <div class="facebook-icon"><a href="<?php if($fbaddr == null) echo "#"; else echo $fbaddr?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/facebook.png"></a></div>
-                        <!--<div class="twitter-icon"><a href="<?php if($twaddr == null) echo "#"; else echo $twaddr; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/twitter.png"></a></div>
+                        <!--<div class="facebook-icon"><a href="<?php if($fbaddr == null) echo "#"; else echo $fbaddr?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/facebook.png"></a></div>
+                        <div class="twitter-icon"><a href="<?php if($twaddr == null) echo "#"; else echo $twaddr; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/twitter.png"></a></div>
                         <div class="pinterest-icon"><a href="<?php if($ptaddr == null) echo "#"; else echo $ptaddr; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/pinterest.png"></a></div>
                         <div class="googlep-icon"><a href="<?php if($gpaddr == null) echo "#"; else echo $gpaddr; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/googlePlus.png"></a></div>
                         <div class="behance-icon"><a href="<?php if($beaddr == null) echo "#"; else echo $beaddr; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/behance.png"></a></div>-->
@@ -211,6 +211,9 @@ function anps_get_header() {
                 <div class="header-email">
                     <div class="mail-icon"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/mail-icon.png"></div>
                     <div class="header-email-text"><a href="mailto:<?php echo $contact_email; ?>"> <p><?php echo $contact_email; ?></p></a></div>
+                    <div class="social-icons-container">
+                        <div class="facebook-icon"><a href="<?php if($fbaddr == null) echo "#"; else echo $fbaddr?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/social-logos/facebook.png"></a></div>
+                    </div>
                 </div>
             </div>
             <div class="container"><?php anps_get_sticky_logo() . anps_get_site_header();?></div>
